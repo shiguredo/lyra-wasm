006a2eeb1125333c0ec3aa75f288f9a7ac5e1316
Takeru Ohta <phjgt308@gmail.com>
2022-11-17 11:49:08 +0900

    Update lyra.cc

--- wasm/lyra.cc
+++ wasm/lyra.cc
@@ -5,7 +5,14 @@
 using namespace emscripten;
 using namespace chromemedia::codec;
 
+absl::Span<const int16_t> makeConstSpan(const int16_t *audio, size_t length) {
+  return absl::MakeConstSpan(audio,length);
+}
+
 EMSCRIPTEN_BINDINGS(lyra_encoder) {
   class_<LyraEncoder>("LyraEcndoer")
-    .class_function("Create", &LyraEncoder::Create);
+    .class_function("CreateFromBuffers", &LyraEncoder::CreateFromBuffers, allow_raw_pointers())
+    .function("Encode", &LyraEncoder::Encode);
+
+  function("makeConstSpan", makeConstSpan, allow_raw_pointers());
 }
