b282fa31193647c9688b4244b430ec22d386c06c
Takeru Ohta <phjgt308@gmail.com>
2022-11-21 10:52:12 +0900

    エンコード関数を追加

--- wasm/lyra.cc
+++ wasm/lyra.cc
@@ -6,13 +6,14 @@
 using namespace emscripten;
 using namespace chromemedia::codec;
 
-absl::Span<const int16_t> makeSpan(std::vector<int16_t> audio_data) {
-  return absl::MakeSpan(audio_data);
-}
-
 std::vector<uint8_t> returnBytes() {
   std::vector<uint8_t> v;
   return v;
+}
+
+std::vector<int16_t> newAudioData(size_t samples) {
+  std::vector<int16_t> data(samples); // TODO
+  return data;
 }
 
 EMSCRIPTEN_BINDINGS(lyra_encoder) {
@@ -20,16 +21,19 @@ EMSCRIPTEN_BINDINGS(lyra_encoder) {
   register_vector<int16_t>("AudioData");
 
   function("returnBytes", &returnBytes);
+  function("newAudioData", &newAudioData);
 
   class_<LyraEncoder>("LyraEncoder")
     .class_function("CreateWithRoot", &LyraEncoder::CreateWithRoot)
-    .class_function("CreateFromBuffers", &LyraEncoder::CreateFromBuffers)
-    .function("Encode", &LyraEncoder::Encode);
-    // .function("Encode", &LyraEncoder::Encode,
-    //           optional_override([](LyraEncoder& self, std::vector<int16_t> audio_data) {
-    //             return self.LyraEncoder::Encode(absl::MakeSpan(audio_data));
-    //           })
-    //           );
-
-  function("makeSpan", makeSpan, allow_raw_pointers());
+    .function("Encode", &LyraEncoder::Encode)
+    .function("Encode2",
+              optional_override([](LyraEncoder& self, std::vector<int16_t> audio_data) {
+                auto result = self.LyraEncoder::Encode(absl::MakeSpan(audio_data));
+                if (result) {
+                  return val(*result);
+                } else {
+                  return val::undefined();
+                }
+              })
+              );
 }
