96637175c287181b79b5855b11a1008f069ab67a
Takeru Ohta <phjgt308@gmail.com>
2022-11-21 11:59:09 +0900

    Add LyraDecoder

--- wasm/lyra.cc
+++ wasm/lyra.cc
@@ -1,7 +1,10 @@
 #include <emscripten/bind.h>
+#include <emscripten/val.h>
+#include <string>
 #include <vector>
 
 #include "lyra_encoder.h"
+#include "lyra_decoder.h"
 
 using namespace emscripten;
 using namespace chromemedia::codec;
@@ -24,9 +27,13 @@ EMSCRIPTEN_BINDINGS(lyra_encoder) {
   function("newAudioData", &newAudioData);
 
   class_<LyraEncoder>("LyraEncoder")
-    .class_function("CreateWithRoot", &LyraEncoder::CreateWithRoot)
-    .function("Encode", &LyraEncoder::Encode)
-    .function("Encode2",
+    .class_function("create",
+                    optional_override([](int sample_rate_hz, int num_channels,
+                                         int bitrate, bool enable_dtx,
+                                         std::string model_path) {
+                      return LyraEncoder::Create(sample_rate_hz, num_channels, bitrate, enable_dtx, model_path);
+                    }))
+    .function("encode",
               optional_override([](LyraEncoder& self, std::vector<int16_t> audio_data) {
                 auto result = self.LyraEncoder::Encode(absl::MakeSpan(audio_data));
                 if (result) {
@@ -36,4 +43,24 @@ EMSCRIPTEN_BINDINGS(lyra_encoder) {
                 }
               })
               );
+
+  class_<LyraDecoder>("LyraDecoder")
+    .class_function("create",
+                    optional_override([](int sample_rate_hz, int num_channels, std::string model_path) {
+                      return LyraDecoder::Create(sample_rate_hz, num_channels, model_path);
+                    }))
+    .function("setEncodedPacket",
+              optional_override([](LyraDecoder& self, std::vector<uint8_t> encoded) {
+                return self.LyraDecoder::SetEncodedPacket(absl::MakeSpan(encoded));
+              }))
+    .function("decodeSamples",
+              optional_override([](LyraDecoder& self, int num_samples) {
+                auto result = self.LyraDecoder::DecodeSamples(num_samples);
+                if (result) {
+                  return val(*result);
+                } else {
+                  return val::undefined();
+                }
+              }))
+    ;
 }
