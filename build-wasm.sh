f2ba9b8d1e93a3e30ee35419a8ab55d03974b5f2
Takeru Ohta <phjgt308@gmail.com>
2022-12-21 09:58:36 +0900

    Pin bazel version to 5.3.2 (until google/lyra supports higher versions)

--- build-wasm.sh
+++ build-wasm.sh
@@ -9,7 +9,7 @@ set -eux
 
 cd wasm/
 pip install --user numpy
-bazel build -c opt --features=-wasm_warnings_as_errors :lyra-wasm
+bazelisk build -c opt --features=-wasm_warnings_as_errors :lyra-wasm
 cd ../
 
 cp wasm/bazel-bin/lyra-wasm/lyra.js src/lyra_wasm.js
