3b54988c46fff47e1ddaf098f88fc2b0ca9a1948
Takeru Ohta <phjgt308@gmail.com>
2022-12-27 14:41:22 +0900

    Remove extra space

--- examples/service-worker.js
+++ examples/service-worker.js
@@ -7,7 +7,7 @@ self.addEventListener('fetch', (e) => {
   const isTarget =
         e.request.url.startsWith('http') &&
         (e.request.url.includes('recording') ||
-         e.request.url.includes('lyra-benchmark')) ;
+         e.request.url.includes('lyra-benchmark'));
   if(!isTarget) {
     e.respondWith(fetch(e.request));
     return;
